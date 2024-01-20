576481e381674bbd56a9cc577120b74afde53af1
Zahra Petiwala <zpetiwal@ucsc.edu>
2024-01-20 10:08:33 -0800

    few changes

--- car_counter/Webcamtest.py
+++ car_counter/Webcamtest.py
@@ -1,4 +1,4 @@
-# import the opencv library 
+# import the opencv library
 import cv2
 import time
 from source_code.vehicle_detector import VehicleDetector
@@ -7,41 +7,39 @@ import requests
 from datetime import datetime
 
 
-# define a video capture object 
-vid = cv2.VideoCapture(0) 
+# define a video capture object
+vid = cv2.VideoCapture(1)
 detector = VehicleDetector()
-url = 'https://bananaspot-249e7-default-rtdb.firebaseio.com/'
+url = "https://bananaspot-249e7-default-rtdb.firebaseio.com/"
 try:
-    while(True): 
-      
-        # Capture the video frame 
-        # by frame 
+    while True:
+        # Capture the video frame
+        # by frame
         ret, frame = vid.read()
         boxes = detector.detect_vehicles(frame)
-        # Display the resulting frame 
+        # Display the resulting frame
         for box in boxes:
             x, y, w, h = box
             cv2.rectangle(frame, (x, y), (x + w, y + h), (25, 0, 180), 3)
-        cv2.imshow('frame', frame)
+        cv2.imshow("frame", frame)
         print("Total current count", len(boxes))
-        # the 'q' button is set as the 
-        # quitting button you may use any 
-        # desired button of your choice 
-        if cv2.waitKey(1) & 0xFF == ord('b'):
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord("b"):
             break
-
 
         now = datetime.now()
 
         current_time = now.strftime("%H:%M:%S")
-        dic = {'cars': len(boxes), 'time': current_time}
+        dic = {"cars": len(boxes), "time": current_time}
         print("Current Time =", current_time)
-        response = requests.put(f'{url}/West_Core/data.json', json.dumps(dic))
-        time.sleep(10)
+        response = requests.put(f"{url}/West_Core/data.json", json.dumps(dic))
+        # time.sleep(10)
 except KeyboardInterrupt:
     pass
-  
-# After the loop release the cap object 
-vid.release() 
-# Destroy all the windows 
-cv2.destroyAllWindows() 
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
--- car_counter/source_code/vehicle_detector.py
+++ car_counter/source_code/vehicle_detector.py
@@ -1,18 +1,16 @@
 import cv2
 import numpy as np
 
-class VehicleDetector:
 
+class VehicleDetector:
     def __init__(self):
         # Load Network
-        net = cv2.dnn.readNet("source_code/dnn_model/yolov4.weights", "source_code/dnn_model/yolov4.cfg")
+        net = cv2.dnn.readNet("dnn_model/yolov4.weights", "dnn_model/yolov4.cfg")
         self.model = cv2.dnn_DetectionModel(net)
         self.model.setInputParams(size=(832, 832), scale=1 / 255)
 
-
         # Allow classes containing Vehicles only
         self.classes_allowed = [2, 3, 5, 6, 7]
-
 
     def detect_vehicles(self, img):
         # Detect Objects
@@ -27,4 +25,3 @@ class VehicleDetector:
                 vehicles_boxes.append(box)
 
         return vehicles_boxes
-
