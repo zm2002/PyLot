8c11fbe5fe3796c1c2b15f72222b328eff983506
Rylee Bao <rvbao@ucsc.edu>
2024-01-21 05:56:46 -0800

    Headers cleaned up

--- ParkingTrackerApp/App.js
+++ ParkingTrackerApp/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
+import { Image } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -9,6 +10,7 @@ import WestRemotePage from './Pages/WestRemote';
 import LoadingScreen from './Pages/Loading';
 import ArtsLotPage from './Pages/ArtLot';
 import WestCorePage from './Pages/WestCore';
+import slugLogo from './slugLogo.png'; // Adjust the path to your image accordingly
 // import EastRemoteSwipe from './Pages/EastRemoteSwipe';
 
 // Firebase imports
@@ -50,8 +52,23 @@ export default function App() {
       },
       headerTitleStyle: {
         fontWeight: 'bold',
+        fontSize: 25,
       },
       headerTintColor: 'white',
+      headerBackTitleVisible: false,
+      headerRight: () => (
+        <Image
+          source={slugLogo}
+          style={{
+            width: 100,
+            height: 100,
+            position: 'absolute', 
+            top: -45,
+            right: -30,
+            zIndex: 10,
+          }} // Adjust the size and margin as needed
+        />
+      ),
       }}>
         <Stack.Screen
           name="Loading Screen"
@@ -64,7 +81,7 @@ export default function App() {
 
         <Stack.Screen 
           name="EastRemote" 
-          options={{ title: 'East Remote' }}
+          options={{ title: 'East Remote', headerBackTitle: null }}
         >
           {(props) => {
             return <EastRemotePage {...props} parkingData={eastRemoteData} />;
@@ -73,7 +90,7 @@ export default function App() {
 
         <Stack.Screen 
           name="WestRemote" 
-          options={{ title: 'West Remote' }}
+          options={{ title: 'West Remote', headerBackTitle: null }}
         >
           {(props) => {
             return <WestRemotePage {...props} parkingData={westRemoteData} />;
